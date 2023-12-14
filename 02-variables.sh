02740a9e0bcfa01095668ce2995b6b39867df043
vinuthna <jainavinuthna@gmail.com>
2023-12-14 19:04:49 +0530

    shell scripts

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello suresh, Good Morning"
+echo "suresh: Hi Ramesh, very Good Morning"
+echo "suresh: How are you Ramesh?"
+echo "Ramesh: I am good suresh, How are you"
