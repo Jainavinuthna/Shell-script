cf823668423d50327a3622cf9aff9cfedd07fc0d
vinuthna <jainavinuthna@gmail.com>
2023-12-15 20:31:35 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh
 PERSON2=suresh
 echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON,  VGood Morning"
-echo "$PERSON2: How are you $PERSON?"
-echo "$PERSON: I am good suresh, How are you"
+echo "$PERSON2: Hi $PERSON1,  VGood Morning"
+echo "$PERSON2: How are you $PERSON1?"
+echo "$PERSON1: I am good suresh, How are you"
 echo "$PERSON2: Thanks"
