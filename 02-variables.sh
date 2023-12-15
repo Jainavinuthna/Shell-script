965db465af3244bf717e46e77b4aad7818e06a82
vinuthna <jainavinuthna@gmail.com>
2023-12-15 14:31:38 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Ramesh: Hello suresh, Good Morning"
-echo "suresh: Hi Ramesh, very Good Morning"
+echo "suresh: Hi Ramesh,  vGood Morning"
 echo "suresh: How are you Ramesh?"
 echo "Ramesh: I am good suresh, How are you"
