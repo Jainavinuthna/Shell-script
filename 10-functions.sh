aa0fd837b83df75e888142fa42d40aaf20564d44
vinuthna <jainavinuthna@gmail.com>
2023-12-18 14:04:01 +0530

    variables

---
+++ 10-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if[ $? -ne 0 ]
+    then
+    echo "echo "Error:installing  failed"
+    exit 1
+    else
+    echo " installling  is sucess"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+echo "Error:please run with root user"
+exit 1
+else
+echo "you are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
