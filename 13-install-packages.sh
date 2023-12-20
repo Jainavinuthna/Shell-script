f8617c5431d70577aa8875a3ad0f387d23d3a31a
vinuthna <jainavinuthna@gmail.com>
2023-12-20 11:58:55 +0530

    shell-script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -6,12 +6,30 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+echo -e"$2..installion $R FAILED"
+else
+echo -e"$2..installation $G SUCESS"
+fi
+}
 if [ $ID -ne 0 ]
 then
 echo -e "$R Error:please run with root user $N"
 exit 1
 else
-echo -e "you are $R root user"
+echo -e "you are $G root user"
 fi
 
-echo "All args passed: $@"
+#echo "All args passed: $@"
+
+for package in $@
+do
+  yum list installed $package
+  if [ $? -ne 0 ]
+  then 
+      yum install $package -y
+      VALIDATE $? "installation of $package"
+
+done      
